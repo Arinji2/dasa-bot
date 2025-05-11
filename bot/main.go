58d0d7233e3e93f7bd9b9f1626f22ca99b1333c0
Arinji2 <arinjaydhar205@gmail.com>
2025-05-12 02:29:35 +0530

    Setup Main Package to work with pbAdmin

--- bot/main.go
+++ bot/main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"time"
+
 	"github.com/arinji2/dasa-bot/env"
+	"github.com/arinji2/dasa-bot/pb"
 )
 
 func main() {
 	e := env.SetupEnv()
+
+	pbAdmin := pb.SetupPocketbase(e.PB)
+
+	ticker := time.NewTicker(5 * time.Hour)
+	defer ticker.Stop()
+
+	// Refreshing Admin Token
+	go func() {
+		for range ticker.C {
+			pbAdmin = pb.SetupPocketbase(e.PB)
+		}
+	}()
 }
