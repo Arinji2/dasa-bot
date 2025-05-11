44dac140ba6f6c5cabea6a4aad188ac1407bcf04
Arinji2 <arinjaydhar205@gmail.com>
2025-05-12 01:55:00 +0530

    Add Branch Fetching

---
+++ bot/pb/branch.go
@@ -0,0 +1,66 @@
+package pb
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/url"
+
+	"github.com/arinji2/dasa-bot/network"
+)
+
+func (p *PocketbaseAdmin) GetAllBranches() ([]BranchCollection, error) {
+	parsedURL, err := url.Parse(p.BaseDomain)
+	if err != nil {
+		return nil, err
+	}
+	parsedURL.Path = "/api/collections/branches/records"
+
+	params := url.Values{}
+
+	parsedURL.RawQuery = params.Encode()
+
+	type request struct{}
+	responseBody, err := network.MakeAuthenticatedRequest(parsedURL, "GET", request{}, p.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	var response PbResponse[BranchCollection]
+	err = json.Unmarshal(responseBody, &response)
+	if err != nil {
+		return nil, err
+	}
+
+	return response.Items, nil
+}
+
+func (p *PocketbaseAdmin) GetBranchByCode(code string) (BranchCollection, error) {
+	parsedURL, err := url.Parse(p.BaseDomain)
+	if err != nil {
+		return BranchCollection{}, err
+	}
+	parsedURL.Path = "/api/collections/branches/records"
+
+	params := url.Values{}
+	params.Add("filter", fmt.Sprintf("code='%s'", code))
+
+	parsedURL.RawQuery = params.Encode()
+
+	type request struct{}
+	responseBody, err := network.MakeAuthenticatedRequest(parsedURL, "GET", request{}, p.Token)
+	if err != nil {
+		return BranchCollection{}, err
+	}
+
+	var response PbResponse[BranchCollection]
+	err = json.Unmarshal(responseBody, &response)
+	if err != nil {
+		return BranchCollection{}, err
+	}
+
+	if len(response.Items) == 0 {
+		return BranchCollection{}, fmt.Errorf("no branch found for code: %s", code)
+	}
+
+	return response.Items[0], nil
+}
