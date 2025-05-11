af4d0a99a9281e9b9e61945716dbe7c05da40bbc
Arinji2 <arinjaydhar205@gmail.com>
2025-05-12 01:20:57 +0530

    Add Bot Env

---
+++ bot/env/env.go
@@ -0,0 +1,56 @@
+package env
+
+import (
+	"log"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Bot struct {
+	Token   string
+	GuildID string
+	ModRole []string
+}
+
+type PB struct {
+	Email      string
+	Password   string
+	BaseDomain string
+}
+type Env struct {
+	Bot Bot
+	PB  PB
+}
+
+func loadEnv(envName string) string {
+	val := os.Getenv(envName)
+	if val == "" {
+		log.Fatalf("Environment variable %s is empty", envName)
+	}
+	return val
+}
+
+func SetupEnv() *Env {
+	log.Println("Loading environment variables...")
+	token := loadEnv("TOKEN")
+	guildID := loadEnv("GUILD_ID")
+	adminEmail := loadEnv("ADMIN_EMAIL")
+	adminPassword := loadEnv("ADMIN_PASSWORD")
+	baseDomain := loadEnv("BASE_DOMAIN")
+	modRole := loadEnv("MOD_ROLE")
+
+	log.Println("Environment variables loaded.")
+	return &Env{
+		Bot: Bot{
+			Token:   token,
+			GuildID: guildID,
+			ModRole: multiEnv(modRole),
+		},
+		PB: PB{
+			Email:      adminEmail,
+			Password:   adminPassword,
+			BaseDomain: baseDomain,
+		},
+	}
+}
---
+++ bot/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/arinji2/dasa-bot/env"
+)
+
+func main() {
+	e := env.SetupEnv()
+}
