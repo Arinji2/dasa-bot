aa7df7e1aad143f04c24b7ab2aabce18be74834c
Arinji2 <arinjaydhar205@gmail.com>
2025-05-12 01:26:18 +0530

    Setup Pocketbase Types

---
+++ bot/pb/types.go
@@ -0,0 +1,46 @@
+package pb
+
+type PocketbaseAdmin struct {
+	Token  string `json:"token"`
+	Record struct {
+		ID string `json:"id"`
+	} `json:"record"`
+	BaseDomain string
+}
+
+type PbResponse[T any] struct {
+	Items      []T `json:"items"`
+	Page       int `json:"page"`
+	PerPage    int `json:"perPage"`
+	TotalItems int `json:"totalItems"`
+	TotalPages int `json:"totalPages"`
+}
+
+type CollegeCollection struct {
+	ID    string `json:"id"`
+	Name  string `json:"name"`
+	Alias string `json:"alias"`
+}
+
+type BranchCollection struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	Code string `json:"code"`
+	Ciwg bool   `json:"ciwg"`
+}
+
+type RankCollection struct {
+	ID         string `json:"id"`
+	Year       int    `json:"year"`
+	Round      int    `json:"round"`
+	JEE_OPEN   int    `json:"jee_open"`
+	JEE_CLOSE  int    `json:"jee_close"`
+	DASA_OPEN  int    `json:"dasa_open"`
+	DASA_CLOSE int    `json:"dasa_close"`
+	College    string `json:"college"`
+	Branch     string `json:"branch"`
+	Expand     struct {
+		College CollegeCollection `json:"college"`
+		Branch  BranchCollection  `json:"branch"`
+	} `json:"expand"`
+}
