733a95961bf3487b1d4add600eecdedf3819920b
Arinji2 <arinjaydhar205@gmail.com>
2025-05-12 11:03:25 +0530

    Use pagination for all colleges

--- bot/bot/college/college.go
+++ bot/bot/college/college.go
@@ -11,18 +11,27 @@ import (
 	"github.com/davecgh/go-spew/spew"
 )
 
+const (
+	collegesPerPage = 10 // Define how many colleges to show per page
+)
+
 type CollegeCommand struct {
 	CollegeData []pb.CollegeCollection
 	PbAdmin     pb.PocketbaseAdmin
 }
 
 func (c *CollegeCommand) HandleCollegeResponse(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	spew.Dump(i)
+	if i.Type == discordgo.InteractionMessageComponent {
+		c.handlePaginationButtons(s, i)
+		return
+	}
+
 	data := i.ApplicationCommandData()
 
-	fmt.Println(data.Options)
 	switch len(data.Options) {
 	case 0:
-		c.handleAllColleges(s, i)
+		c.handleAllColleges(s, i, 0)
 	case 1:
 		c.handleSpecificColleges(s, i, data)
 	}
@@ -68,24 +77,138 @@ func (c *CollegeCommand) HandleCollegeAutocomplete(s *discordgo.Session, i *disc
 	}
 }
 
-func (c *CollegeCommand) handleAllColleges(s *discordgo.Session, i *discordgo.InteractionCreate) {
-	description := "Showing **All Colleges** \n"
+func (c *CollegeCommand) handleAllColleges(s *discordgo.Session, i *discordgo.InteractionCreate, page int) {
 	if len(c.CollegeData) == 0 {
-		commands_utils.RespondWithEphemeralError(s, i, "No articles found ")
+		commands_utils.RespondWithEphemeralError(s, i, "No colleges found")
 		return
 	}
 
-	for i, v := range c.CollegeData {
-		if i > 20 {
-			break
-		}
-		description += fmt.Sprintf(
-			"%d. **Name: ** %s \n\n **Alias: ** %s\n\n", i+1, v.Name, v.Alias)
+	totalPages := (len(c.CollegeData) + collegesPerPage - 1) / collegesPerPage
+
+	if page < 0 {
+		page = 0
+	} else if page >= totalPages {
+		page = totalPages - 1
 	}
 
-	fmt.Println(description)
+	startIdx := page * collegesPerPage
+	endIdx := startIdx + collegesPerPage
+	endIdx = min(endIdx, len(c.CollegeData))
 
-	commands_utils.RespondWithEmbed(s, i, "Dasa College Details", description)
+	description := fmt.Sprintf("Showing **All Colleges** (Page %d/%d)\n\n", page+1, totalPages)
+
+	for i, v := range c.CollegeData[startIdx:endIdx] {
+		description += fmt.Sprintf(
+			"%d. **Name: ** %s \n\n **Alias: ** %s\n\n", startIdx+i+1, v.Name, v.Alias)
+	}
+
+	components := []discordgo.MessageComponent{
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+				discordgo.Button{
+					Label:    "Previous",
+					Style:    discordgo.PrimaryButton,
+					CustomID: fmt.Sprintf("college_prev_%d", page),
+					Disabled: page <= 0,
+				},
+				discordgo.Button{
+					Label:    fmt.Sprintf("Page %d/%d", page+1, totalPages),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "college_page_info",
+					Disabled: true,
+				},
+				discordgo.Button{
+					Label:    "Next",
+					Style:    discordgo.PrimaryButton,
+					CustomID: fmt.Sprintf("college_next_%d", page),
+					Disabled: page >= totalPages-1,
+				},
+			},
+		},
+	}
+
+	err := commands_utils.RespondWithEmbedAndComponents(s, i, "Dasa College Details", description, components)
+	if err != nil {
+		log.Printf("Error sending college response: %v", err)
+	}
+}
+
+func (c *CollegeCommand) handlePaginationButtons(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	customID := i.MessageComponentData().CustomID
+	var targetPage int
+
+	if strings.HasPrefix(customID, "college_prev_") {
+		fmt.Sscanf(customID, "college_prev_%d", &targetPage)
+		targetPage--
+	} else if strings.HasPrefix(customID, "college_next_") {
+		fmt.Sscanf(customID, "college_next_%d", &targetPage)
+		targetPage++
+
+		s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+			Type: discordgo.InteractionResponseDeferredMessageUpdate,
+		})
+		return
+	}
+
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseDeferredMessageUpdate,
+	})
+	if err != nil {
+		log.Printf("Error deferring response: %v", err)
+		return
+	}
+
+	totalPages := (len(c.CollegeData) + collegesPerPage - 1) / collegesPerPage
+
+	if targetPage < 0 {
+		targetPage = 0
+	} else if targetPage >= totalPages {
+		targetPage = totalPages - 1
+	}
+
+	startIdx := targetPage * collegesPerPage
+	endIdx := startIdx + collegesPerPage
+	endIdx = min(endIdx, len(c.CollegeData))
+
+	description := fmt.Sprintf("Showing **All Colleges** (Page %d/%d)\n\n", targetPage+1, totalPages)
+
+	for i, v := range c.CollegeData[startIdx:endIdx] {
+		description += fmt.Sprintf(
+			"%d. **Name: ** %s \n\n **Alias: ** %s\n\n", startIdx+i+1, v.Name, v.Alias)
+	}
+
+	components := []discordgo.MessageComponent{
+		discordgo.ActionsRow{
+			Components: []discordgo.MessageComponent{
+				discordgo.Button{
+					Label:    "Previous",
+					Style:    discordgo.PrimaryButton,
+					CustomID: fmt.Sprintf("college_prev_%d", targetPage),
+					Disabled: targetPage <= 0,
+				},
+				discordgo.Button{
+					Label:    fmt.Sprintf("Page %d/%d", targetPage+1, totalPages),
+					Style:    discordgo.SecondaryButton,
+					CustomID: "college_page_info",
+					Disabled: true,
+				},
+				discordgo.Button{
+					Label:    "Next",
+					Style:    discordgo.PrimaryButton,
+					CustomID: fmt.Sprintf("college_next_%d", targetPage),
+					Disabled: targetPage >= totalPages-1,
+				},
+			},
+		},
+	}
+
+	_, err = s.InteractionResponseEdit(i.Interaction, &discordgo.WebhookEdit{
+		Embeds:     &[]*discordgo.MessageEmbed{commands_utils.CreateBaseEmbed("Dasa College Details", description)},
+		Components: &components,
+	})
+	if err != nil {
+		log.Printf("Error editing college response: %v", err)
+	}
 }
 
 func (c *CollegeCommand) handleSpecificColleges(s *discordgo.Session, i *discordgo.InteractionCreate, data discordgo.ApplicationCommandInteractionData) {
@@ -103,7 +226,7 @@ func (c *CollegeCommand) handleSpecificColleges(s *discordgo.Session, i *discord
 
 	description += fmt.Sprintf(
 		"**Alias:** %s\n\n ", collegeData.Alias)
-	err = commands_utils.RespondWithEmbed(s, i, "DASA CollegeDetails", description)
+	err = commands_utils.RespondWithEmbed(s, i, "DASA College Details", description)
 	if err != nil {
 		log.Printf("Error sending college response: %v", err)
 	}
